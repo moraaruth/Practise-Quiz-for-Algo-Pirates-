24b5e7620ba5ca639ee286625b62df9d69325ead
Ruth Moraa <moraar37@gmail.com>
2022-03-10 17:16:44 +0300

    a function that converts fahrenhit to celsius

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ let earth = {
     }
     
     console.log(earth.small);
+
+
+    let celsius = document.getElementById('celsius');
+    let fahrengeit = document.getElementById('fahrengeit');
+
+    celsius.oninput = () => {
+        let output = (parseFloat(celsius.value)* 9) / 5 + 32;
+        fahrengeit.value = parseFloat(output.toFixed(2));
+
+
+    };
+
+    fahrengeit.oninput = () => {
+        let output = ((parseFloat(fahrengeit.value) - 32) * 5) / 9;
+        celsius.value = parseFloat(output.toFixed(2));
+
+
+    };
+
+
